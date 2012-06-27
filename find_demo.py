1b0255221be22c0444b23069500ef6b24be81dd8
Jonathan <jmportug@umich.edu>
2012-06-27 13:12:40 -0500

    Added a unit test file

--- find_demo.py
+++ find_demo.py
@@ -20,7 +20,7 @@ from find import (	find,
   
 
 ctree = tree()
-ctree.generate(ctree.root,5)
+ctree.generate(ctree.root)
 print(ctree.size)
 print(ctree.num_leaves)
 
--- tree.py
+++ tree.py
@@ -12,9 +12,16 @@ class tree:
 		self.leaves={}
 		
 	def generate(self, cnode, max_depth=7, depth=0, dir_name="/1"):
+#		if(depth+1 >= max_depth):
+#			self.num_leaves += 1
+#			self.leaves.update({dir_name+"/"+str(self.seiz):cnode})
+
+#		print(len(cnode.sub_dirs))
 		if(depth < max_depth):
+#		else:
+
 			i=0
-			while(i<5):
+			while(len(cnode.sub_dirs)<5):
 				self.size+=1
 				name = dir_name+"/"+str(self.size)
 				cnode.sub_dirs.append(node(name))
@@ -22,5 +29,6 @@ class tree:
 				i+=1
 				
 		if(len(cnode.sub_dirs) == 0):
-			self.num_leaves += 1
+#		else: 
+                        self.num_leaves += 1
 			self.leaves.update({dir_name+"/"+str(self.size):cnode})
